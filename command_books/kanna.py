930b3ef330eb40ca3b0a4ee31191b1311c548654
Nekololizu <nekololizu@gmail.com>
2022-05-26 22:30:06 +0200

    lucid soul

--- command_books/kanna.py
+++ command_books/kanna.py
@@ -25,6 +25,7 @@ class Key:
     # Skills
     SHIKIGAMI = 'r'
     TENGU = 'q'
+    LUCID_SOUL = '1'
     YAKSHA = '2'
     VANQUISHER = 'f'
     KISHIN = 'ctrl'
@@ -216,6 +217,30 @@ class Yaksha(Command):
             else:
                 press('right', 1, down_time=0.1, up_time=0.05)
         press(Key.YAKSHA, 3)
+
+
+class LucidSoul(Command):
+    """
+    Places 'Lucid' in a given direction, or towards the center of the map if
+    no direction is specified.
+    """
+
+    def __init__(self, direction=None):
+        super().__init__(locals())
+        if direction is None:
+            self.direction = direction
+        else:
+            self.direction = settings.validate_horizontal_arrows(direction)
+
+    def main(self):
+        if self.direction:
+            press(self.direction, 1, down_time=0.1, up_time=0.05)
+        else:
+            if config.player_pos[0] > 0.5:
+                press('left', 1, down_time=0.1, up_time=0.05)
+            else:
+                press('right', 1, down_time=0.1, up_time=0.05)
+        press(Key.LUCID_SOUL, 3)
 
 
 class Vanquisher(Command):
