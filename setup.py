cf95d98a44fc0177427c8ea0d2fabeadc1c9d762
Jeffrey Tan <tanjeffreyz02@berkeley.edu>
2022-05-26 19:27:05 -0700

    code cleanup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import win32com.client as client
 
 def create_desktop_shortcut():
     """Creates and saves a desktop shortcut using absolute paths"""
-    print('\n[~] Creating a desktop shortcut for Auto Maple:')
+    print('\n[~] Creating desktop shortcut for Auto Maple:')
     CWD = os.getcwd()
     TARGET = os.path.join(os.environ['WINDIR'], 'System32', 'cmd.exe')
     PATH = os.path.join(os.environ['USERPROFILE'], 'Desktop', 'Auto Maple.lnk')
